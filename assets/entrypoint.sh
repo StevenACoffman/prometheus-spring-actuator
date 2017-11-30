766e3de4e311da6d74f389d8f1caf02f3343288b
Steve Coffman <StevenACoffman@users.noreply.github.com>
2017-11-30 15:00:42 -0500

    Add additional JMX options

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -7,7 +7,15 @@ fi
 
 if [ "$JMX" = true ]; then
   printf "Running the application in JMX mode\n"
-  JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+  JMX_OPTS=(
+    "-Dcom.sun.management.jmxremote"
+    "-Dcom.sun.management.jmxremote.port=$JMX_PORT"
+    "-Dcom.sun.management.jmxremote.local.only=false"
+    "-Dcom.sun.management.jmxremote.authenticate=false"
+    "-Dcom.sun.management.jmxremote.ssl=false"
+  )
+  
+  JAVA_OPTS="$JAVA_OPTS ${JMX_OPTS[@]}"
 fi
 printf "Running APP_HOME/JAR_FILE $APP_HOME/$JAR_FILE\n"
 # Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
